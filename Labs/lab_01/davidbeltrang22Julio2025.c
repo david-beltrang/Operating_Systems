fea928ac39e3cd3d4222bc2ee0ad3a0ccb9d1b03
David Beltran Gomez <estedavid0104@gmail.com>
2025-08-26 02:15:56 -0500

    First test with nano and .c files.

---
+++ Labs/lab_01/davidbeltrang22Julio2025.c
@@ -0,0 +1,30 @@
+/*******************************************************************************
+|        ------         Pontificia Universidad Javeriana       -------        |
+|                                                                             |
+|      Autor: Beltran Gomez David                                             |
+|      Fecha: 22 de Julio 2025                                                |
+|      Materia: Sistemas Operativos                                           |
+|      Docente: John Corredor                                                 |
+|      Objetivo: Programacion de C(Introduccion)                              |
+|                                                                             |
+*******************************************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+        printf("\n\n\t***************************\n");
+        printf("\t**  Sistemas Operativos  **\n");
+        printf("\t*                         *\n");
+        printf("\t**  David Beltran Gomez  **\n");
+        printf("\t***************************\n\n");
+        printf("\t***************************\n");
+        printf("\t*       ___      ___      *\n"); 
+        printf("\t*        O        O       *\n"); 
+        printf("\t*         \\______/        *\n"); 
+        printf("\t*                         *\n");
+        printf("\t*          FELIZ          *\n");  
+        printf("\t***************************\n"); 
+        return 0;
+}
